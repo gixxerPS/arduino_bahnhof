f4aa5b0d03a1928c24dfc4a3dc4557006fbb813e
Daniel Boenke <d.boenke@gmx.net>
2024-12-14 09:45:41 +0100

    vereinfachte sequenz

--- drehscheibe/include/sequence.h
+++ drehscheibe/include/sequence.h
@@ -9,11 +9,17 @@ namespace SEQ
     const unsigned long T_VERZ_START_F_DREHR = 10000; // [ms] verzoegerung startbefehl fuer drehrichtungswahl
     const unsigned long T_STOER_ANZ = 10000; // [ms] anzeigedauer stoerung
     const unsigned long T_STOER_IMPULS = 500; // [ms] anzeigedauer stoerung
+    const unsigned long T_VERZ_NACH_TOR_AUF = 4000; // [ms] verzoegerung fuer fahrstrom nachdem tor geoeffnet wurde
     
     enum RICHTUNG {
         KEINE = 0,
         LINKS = 1,
         RECHTS = 2
+    };
+    enum HOLBRING {
+        KEIN = 0,
+        HOLEN = 1,
+        BRINGEN = 2
     };
     
     extern uint16_t step;
--- drehscheibe/src/sequence.cpp
+++ drehscheibe/src/sequence.cpp
@@ -13,7 +13,8 @@ namespace SEQ
 
     int8_t zielGleis;
     int8_t aktGleisPos; // aktuelle position
-    RICHTUNG richtung; // 0=aus, 1=links, 2=rechts
+    RICHTUNG richtung; // wie rum soll gedreht werden
+    HOLBRING holbringAuftrag; 
 
     void loop()
     {
@@ -36,24 +37,21 @@ namespace SEQ
                     DO::allOutsOff();
                     zielGleis = 0;
                     richtung = RICHTUNG::KEINE;
+                    holbringAuftrag = HOLBRING::KEIN;
                 }
 
                 // pruefe anwahl
                 for (uint8_t i = 1; i <= ANZ_GLEISE; i++) { // 1..12 gleis num, 13=A, 14=B, 15=C
                     if (DI::getInGLEIS_ANWAHL(i)) { // anwahltaste gedrueckt ?
                         if (zielGleis == 0) { 
-                            if (DI::getInGLEIS_BESETZT(i)) {
-                                step = 999;
-                            } else { // gleis noch nicht besetzt
-                                zielGleis = i;
-                                DO::setOutStandrelais(i, HIGH); // und anzeigen
-                                if (zielGleis >= ERSTES_STANDGLEIS && zielGleis <= LETZTES_STANDGLEIS) {
-                                    step = 100;
-                                } else if (zielGleis >= ERSTES_EINFAHRTSGLEIS && zielGleis <= LETZTES_EINFAHRTSGLEIS) {
-                                    step = 200;
-                                } else if (zielGleis >= ERSTES_AUSFAHRTSGLEIS && zielGleis <= LETZTES_AUSFAHRTSGLEIS) {
-                                    step = 300;
-                                }
+                            zielGleis = i;
+                            DO::setOutStandrelais(i, HIGH); // und anzeigen
+                            if (zielGleis >= ERSTES_STANDGLEIS && zielGleis <= LETZTES_STANDGLEIS) {
+                                step = 100;
+                            } else if (zielGleis >= ERSTES_EINFAHRTSGLEIS && zielGleis <= LETZTES_EINFAHRTSGLEIS) {
+                                step = 100; // 200; doch gleiche sequenz moeglich
+                            } else if (zielGleis >= ERSTES_AUSFAHRTSGLEIS && zielGleis <= LETZTES_AUSFAHRTSGLEIS) {
+                                step = 100; // 300; doch gleiche sequenz moeglich
                             }
                         } 
                     }
@@ -61,30 +59,39 @@ namespace SEQ
             break;
 
 //=============================================================================
-// Drehen auf Standgleis
+// Drehen auf Standgleis zum abholen ODER wegbringen
 //=============================================================================
             case 100: // besetzt pruefen
-                if (DI::getInBUEHNE_BESETZT() ) {
-                    step = 999;
-                } else if (DI::getInGLEIS_BESETZT(zielGleis)) {
-                    step = 999;
-                } else { // frei, ok weiter
+                if (DI::getInBUEHNE_BESETZT() && !DI::getInGLEIS_BESETZT(zielGleis)) { // wegbringen
+                    holbringAuftrag = HOLBRING::BRINGEN;
                     step = 110;
+                } else if (!DI::getInBUEHNE_BESETZT() && DI::getInGLEIS_BESETZT(zielGleis)) { // abholen
+                    holbringAuftrag = HOLBRING::HOLEN;
+                    step = 110; 
+                } else { // buehne und stand besetzt, stoerung
+                    step = 999;
                 }
             break;
             case 110: // standrelais an, drehrichtung ermitteln
-                DO::setOutStandrelais(zielGleis, HIGH);
-                if (aktGleisPos == 0) {
-                    richtung = RICHTUNG::LINKS; // default = links
-                    step = 120;
-                } else if (aktGleisPos - zielGleis > 0) {
-                    richtung = RICHTUNG::LINKS;
-                    step = 120;
-                } else if (aktGleisPos - zielGleis < 0) {
-                    richtung = RICHTUNG::RECHTS;
-                    step = 120;
-                } else { // aktGleisPos - zielGleis = 0, also ziel = istposition: error
-                    step = 999;
+                if (aktGleisPos != zielGleis) {
+                    DO::setOutStandrelais(zielGleis, HIGH);
+                    if (aktGleisPos == 0) {
+                        richtung = RICHTUNG::LINKS; // default = links
+                        step = 120;
+                    } else if (aktGleisPos - zielGleis > 0) {
+                        richtung = RICHTUNG::LINKS;
+                        step = 120;
+                    } else if (aktGleisPos - zielGleis < 0) {
+                        richtung = RICHTUNG::RECHTS;
+                        step = 120;
+                    } else { // aktGleisPos - zielGleis = 0, also ziel = istposition: error
+                        step = 999;
+                    }
+                } else { // aktGleisPos == zielGleis
+                    if (zielGleis >= ERSTES_STANDGLEIS && zielGleis <= LETZTES_STANDGLEIS) {
+                        DO::setOutZielTorRelais(zielGleis, HIGH);
+                    }
+                    step = 140;
                 }
             break;
             case 120: // manuelle drehrichtungsaenderung moeglich
@@ -101,7 +108,41 @@ namespace SEQ
                 }
             break;
             case 130: // drehen
-                // TODO: continue here
+                if (richtung == RICHTUNG::LINKS) {
+                    DO::setOutLinks(HIGH);
+                } else if (richtung == RICHTUNG::RECHTS) {
+                    DO::setOutRechts(HIGH);
+                }
+                DO::setOutStart(HIGH);
+
+                // tor oeffnen, wenn nah genug am ziel
+                if (abs(zielGleis - aktGleisPos) < 3) { // bsp. ziel=5, aktGleisPos=7 => tor 5 auf
+                    // aber nur bei standgleis
+                    if (zielGleis >= ERSTES_STANDGLEIS && zielGleis <= LETZTES_STANDGLEIS) {
+                        DO::setOutZielTorRelais(zielGleis, HIGH);
+                    }
+                }
+                if (DI::getInGLEIS_POSITION(zielGleis)) {
+                    DO::setOutLinks(LOW);
+                    DO::setOutRechts(LOW);
+                    DO::setOutStart(LOW);
+                    step = 140;
+                }
+            break;
+            case 140: // fahrstrom an
+                if (millis() - stepStartMillis > T_VERZ_NACH_TOR_AUF) {
+                    DO::setOutFahrstrom(HIGH);
+                    if (holbringAuftrag == HOLBRING::HOLEN) {
+                        step = 150;
+                    } else { // wegbringen
+                        // warte auf reset durch bediener
+                    }
+                }
+            break;
+            case 150: // warte auf auffahrt auf buehne
+                if (DI::getInBUEHNE_BESETZT()) {
+                    step = 0; // fertig, alles aus
+                }
             break;
 //=============================================================================
 // Drehen auf Einfahrtsgleis
@@ -135,7 +176,28 @@ namespace SEQ
 //=============================================================================
 // Drehen auf Ausfahrtsgleis
 //=============================================================================
-            case 300: 
+            case 300: // besetzt pruefen
+                if (!DI::getInBUEHNE_BESETZT() ) {
+                    step = 999;
+                } else if (DI::getInGLEIS_BESETZT(zielGleis)) {
+                    step = 999;
+                } else { // frei, ok weiter
+                    step = 310;
+                }
+            break;
+            case 310: // tor oeffnen
+                DO::setOutZielTorRelais(aktGleisPos, HIGH);
+                if (millis() - stepStartMillis > T_VERZ_NACH_TOR_AUF) {
+                    step = 320;
+                }
+            break;
+            case 320: // auf buehne fahren
+                DO::setOutFahrstrom(HIGH);
+                if (DI::getInBUEHNE_BESETZT()) {
+                    DO::setOutFahrstrom(LOW);
+                    DO::setOutZielTorRelais(aktGleisPos, LOW);
+                    DO::setOutStandrelais(aktGleisPos, LOW);
+                }
             break;
 //=============================================================================
 // stoerung
@@ -165,6 +227,10 @@ namespace SEQ
             // if (step > 0 && step != lastStep) { // neuer schritt ungleich init ?
             //     lastStep = step;
             // }
+            Serial.print(F("Schrittwechsel! aktuell: "));
+            Serial.print(step);
+            Serial.print(F(" | letzer: "));
+            Serial.print(lastStep);
         }
         stepChange = step != stepOld;
         stepOld = step;
